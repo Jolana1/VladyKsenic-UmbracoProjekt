26e0904406c6adf847695a8cc8398018903a7af7
Jolana1 <ksenicv@icloud.com>
2023-03-27 21:05:51 +0200

    27.3.23 doplnenie EmailTemplates a Kurzu vo vzdelaniemodel

--- VladyKsenicUmbracoProjekt.lib/Models/Kurzy.cs
+++ VladyKsenicUmbracoProjekt.lib/Models/Kurzy.cs
@@ -8,6 +8,6 @@ namespace VladyKsenicUmbracoProjekt.lib.Models
 {
     public class Kurzy
     {
-        public string[] zoznamKurzov = { "Kurz Html", "Kurz CSS", "Kurz JS", "Kurz FrontEndDeveloper", "Kurz DeveloperBoss", "Kurz Python", "Kurz SQL", "Kurz Java pre zaciatocníkov", "A iné" };
+        public string[] zoznamKurzov = { "Kurz Html 3,5", "Kurz CSS", "Kurz JS", "Kurz FrontEndDeveloper", "Kurz DeveloperBoss", "Kurz Python", "Kurz SQL", "Kurz Java pre zaciatocníkov", "Windows Administrator" };
     }
 }
