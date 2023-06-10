2c000b68407f957c7218af220aa8b4faa348d2ad
Jolana1 <ksenicv@icloud.com>
2023-06-10 17:01:55 +0200

    10.6.23

--- VladyKsenicUmbracoProjekt.lib/Models/Kurzy.cs
+++ VladyKsenicUmbracoProjekt.lib/Models/Kurzy.cs
@@ -8,6 +8,6 @@ namespace VladyKsenicUmbracoProjekt.lib.Models
 {
     public class Kurzy
     {
-        public string[] zoznamKurzov = { "Kurz Html 3,5", "Kurz CSS", "Kurz JS", "Kurz FrontEndDeveloper", "Kurz DeveloperBoss", "Kurz Python", "Kurz SQL", "Kurz Java pre zaciatocníkov", "Windows Administrator" };
+        public string[] zoznamKurzov = { "Kurz Windows Administrator", "Kurz CSS", "Kurz JS", "Kurz FrontEndDeveloper", "Kurz DeveloperBoss", "Kurz Python", "Kurz SQL", "Kurz Java pre zaciatocníkov", };
     }
 }
